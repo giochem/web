8fb64af2683136243faa343ba74319834b6c0ddd
gio chem <tranxuantruong203hn@gmail.com>
2022-01-22 16:59:44 +0700

    first commit

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ function getRemaindingTime(){
   const today = new Date().getTime();
   const t = futureTime - today;
   
-  // 1m = 60s
+  
   // 1hr = 60m
   // 1d = 24hr
   const oneDay = 24 * 60 * 60 * 1000;
